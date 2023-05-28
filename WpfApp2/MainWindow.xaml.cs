f5a24dbc6657705c27a0348dad2e6c5baf595c50
Hesen_ch75 <Hesen_ch75@STHQ0124-17>
2023-05-28 14:05:13 +0400

    teze forma

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -32,22 +32,81 @@ namespace WpfApp2
             InitializeComponent();
             conn = new SqlConnection();
             cs = ConfigurationManager.ConnectionStrings["myconn"].ConnectionString;
-            using (conn=new SqlConnection())
-            {
-                var da = new SqlDataAdapter();
-                conn.ConnectionString = cs;
-                conn.Open();
-                var set = new DataSet();
-                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
-                da.SelectCommand = command;
-                da.Fill(set,"AuthorsSet");
-                mydataGrid1.ItemsSource = set.Tables[0].DefaultView;
-            }
+            //using (conn=new SqlConnection())
+            //{
+            //    var da = new SqlDataAdapter();
+            //    conn.ConnectionString = cs;
+            //    conn.Open();
+            //    var set = new DataSet();
+            //    SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+            //    da.SelectCommand = command;
+            //    da.Fill(set,"AuthorsSet");
+            //    mydataGrid1.ItemsSource = set.Tables[0].DefaultView;
+            //}
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //#region DataTable
+            //using(var conn=new SqlConnection())
+            //{
 
+
+            //    conn.ConnectionString = cs;
+            //    conn.Open();
+            //    SqlCommand command = new SqlCommand();
+            //    command.CommandText = "SELECT * FROM Authors";
+            //    command.Connection = conn;  
+            //    table = new DataTable();    
+                
+            //    bool hasColumnAdded=false;
+            //    using (reader = command.ExecuteReader())
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            if (!hasColumnAdded)
+            //            {
+            //                for (int i = 0; i < reader.FieldCount; i++)
+            //                {
+            //                    table.Columns.Add(reader.GetName(i));
+
+            //                }
+            //                hasColumnAdded = true;
+            //            }
+            //            DataRow row = table.NewRow();
+            //            for (int i = 0; i < reader.FieldCount; i++)
+            //            {
+            //                row[i] = reader[i];
+            //            }
+            //            table.Rows.Add(row);
+            //        }
+            //        mydataGrid1.ItemsSource = table.DefaultView;
+
+
+            //    }
+
+
+
+            //}
+
+
+
+            //#endregion
+
+            #region Dataset And SqlDataAdaTer
+            using (conn=new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+                var set = new DataSet();
+                var da = new SqlDataAdapter("SELECT * FROM Authors; SELECT *FROM Books",conn);
+                da.Fill(set);
+                mydataGrid1.ItemsSource = set.Tables[0].DefaultView;
+                mydataGrid2.ItemsSource = set.Tables[0].DefaultView;
+            }
+
+
+            #endregion
         }
     }
 }
