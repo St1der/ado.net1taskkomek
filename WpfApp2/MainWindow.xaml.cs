9cfdeb169dc1bfd4e5039613bb1804ffef0ed4c5
Hesen_ch75 <Hesen_ch75@STHQ0124-17>
2023-05-28 13:24:48 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn;
+        string cs = "";
+        DataTable table;
+        SqlDataReader reader;
+        public MainWindow()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["myconn"].ConnectionString;
+            using (conn=new SqlConnection())
+            {
+                var da = new SqlDataAdapter();
+                conn.ConnectionString = cs;
+                conn.Open();
+                var set = new DataSet();
+                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+                da.SelectCommand = command;
+                da.Fill(set,"AuthorsSet");
+                mydataGrid1.ItemsSource = set.Tables[0].DefaultView;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
